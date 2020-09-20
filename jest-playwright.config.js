800f42338fc2c6d8752a3bf9f0f96d7178d0ba3e
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-09-20 12:14:03 -0400

    add jest configs

---
+++ jest-playwright.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  browsers: ["chromium", "firefox", "webkit"],
+  launchOptions: {
+    headless: true,
+  },
+  devices: ["iPhone 8", "iPhone XS Max", "iPad", "Pixel 2", "Pixel 2 XL"],
+};
