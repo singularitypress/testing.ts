38c7f479b5805d6f88daa953b35417a1c8a8fbc9
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-09-20 13:20:21 -0400

    need to only run video on chromium, seperate tests/configs maybe

--- tests/samples/github.test.ts
+++ tests/samples/github.test.ts
@@ -1,3 +1,5 @@
+import { saveVideo } from "playwright-video";
+
 describe("GitHub", () => {
   it("should show the singularitypress/ts-ssr-kit project in the search if you search for it", async () => {
     await page.goto("https://github.com/singularitypress/ts-ssr-kit");
@@ -6,6 +8,7 @@ describe("GitHub", () => {
     // await expect(page).toHaveText(".repo-list", "singularitypress/ts-ssr-kit");
   });
   it("should contain 'How the sausage is made' in the project title", async () => {
+    const capture = await saveVideo(page, "tmp/video.mp4");
     // await page.click(".repo-list-item:nth-child(1) a");
     // via the CSS selector
     await expect(page).toHaveText("#readme h1", "How the sausage is made");
@@ -14,5 +17,6 @@ describe("GitHub", () => {
     await expect(page).toHaveSelector("#user-content-how-the-sausage-is-made", {
       state: "attached",
     });
+    await capture.stop();
   });
 });
