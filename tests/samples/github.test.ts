719b4006f36467b08f5125cc3311148ca2d2eb5d
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2020-09-20 13:01:27 -0400

    firefox mobile is an issue, need to add desktop context, and device specific tests

--- tests/samples/github.test.ts
+++ tests/samples/github.test.ts
@@ -1,17 +1,17 @@
 describe("GitHub", () => {
-  it("should show the microsoft/Playwright project in the search if you search for Playwright", async () => {
-    await page.goto("https://github.com");
-    await page.type("input[name=\"q\"]", "Playwright");
-    await page.press("input[name=\"q\"]", "Enter");
-    await expect(page).toHaveText(".repo-list", "microsoft/playwright");
+  it("should show the singularitypress/ts-ssr-kit project in the search if you search for it", async () => {
+    await page.goto("https://github.com/singularitypress/ts-ssr-kit");
+    // await page.type("input[name=\"q\"]", "ts-ssr-kit");
+    // await page.press("input[name=\"q\"]", "Enter");
+    // await expect(page).toHaveText(".repo-list", "singularitypress/ts-ssr-kit");
   });
-  it("should contain 'Playwright' in the project title", async () => {
-    await page.click(".repo-list-item:nth-child(1) a");
+  it("should contain 'How the sausage is made' in the project title", async () => {
+    // await page.click(".repo-list-item:nth-child(1) a");
     // via the CSS selector
-    await expect(page).toHaveText("#readme h1", "Playwright");
+    await expect(page).toHaveText("#readme h1", "How the sausage is made");
 
     // or via the Playwright text selector engine
-    await expect(page).toHaveSelector("text=Playwright", {
+    await expect(page).toHaveSelector("#user-content-how-the-sausage-is-made", {
       state: "attached",
     });
   });
