a97d69e397f3cd37902fa52b44d6e9ba3d8fe769
singularitypress <jaypandya2k5@gmail.com>
2020-09-24 18:49:03 -0400

    updated AEM test to do something similar to ssr

---
+++ util/device-constants.ts
@@ -0,0 +1,8 @@
+export const desktop = ["chromium", "firefox", "webkit"] as IBrowsers;
+export const mobile = [
+  "iPhone SE",
+  "iPhone 11 Pro Max",
+  "iPhone 11",
+  "Pixel 2",
+  "Pixel 2 XL",
+] as IDevices;
